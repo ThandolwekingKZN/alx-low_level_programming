1b408a7a2433570d7d4fd62707df39ffd1924386
Thandolwenkosi <info.ludonga@gmail.com>
2023-07-16 11:29:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ * main - Program that prints using puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
